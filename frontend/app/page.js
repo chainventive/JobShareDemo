ce905dc741aaa425b5bda649d1f124017fea1b30
chainventive <chainventive@gmail.com>
2023-11-14 19:12:44 +0100

    Added jobs component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,11 @@
 import Header from "@/components/header/Header"
+import Jobs from "@/components/jobs/Jobs"
 
 export default function Home() {
   return (
     <main>
       <Header/>
+      <Jobs/>
     </main>
   )
 }
---
+++ frontend/components/jobs/Jobs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Flex, Center } from '@chakra-ui/react'
+
+const Jobs = () => {
+  return (
+    <>
+        <Flex p="0.5rem">
+            <Center width='100%'>
+
+                - JOB COMPONENT -
+
+            </Center>
+        </Flex>
+    </>
+  )
+}
+
+export default Jobs
