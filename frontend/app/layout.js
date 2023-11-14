5dcbaeb258f4bcff5e1abfb3b97ee5d12b1d58a3
chainventive <chainventive@gmail.com>
2023-11-14 19:01:45 +0100

    Installed frontend dependancies

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,8 +1,42 @@
+"use client"
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { hardhat } from 'wagmi/chains';
+
+const { chains, publicClient } = configureChains(
+  [hardhat],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'alyra-dev',
+  projectId: 'f72debe8724f32506362ef7f21095eea',
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: false,
+  connectors,
+  publicClient
+})
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <WagmiConfig config={wagmiConfig}>
+          <RainbowKitProvider chains={chains}>
+              <ChakraProvider>
+                {children}
+              </ChakraProvider>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </body>
     </html>
   )
 }
